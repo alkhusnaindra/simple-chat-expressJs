064c34881431c5f5598813644880931b2bfde7fd
Indra Maulana <alkhusnaindra1@gmail.com>
2025-02-04 22:17:50 +0700

    update lagi teros

--- src/index.js
+++ src/index.js
@@ -72,17 +72,25 @@ io.on("connection", (socket) => {
         `Received message from ${senderId} to ${receiverId}: ${content}`
       );
 
+      // Menyimpan pesan di database
       const message = await prisma.message.create({
-        data: { senderId, receiverId, content },
+        data: {
+          senderId,
+          receiverId,
+          content,
+        },
       });
 
+      // Cek apakah penerima (receiver) sedang online
       const receiverSocketId = usersOnline.get(receiverId);
       if (!receiverSocketId) {
         console.log(
           `User ${receiverId} is offline, message saved in database.`
         );
-        return;
+        return; // Tidak mengirim pesan ke socket karena offline
       }
+
+      // Kirim pesan ke penerima jika mereka online
       io.to(receiverSocketId).emit("receiveMessage", message);
     } catch (error) {
       console.error("Error sending message:", error);
